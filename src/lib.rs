5fab4e622601a36243dd331d2c6f9b7e818a72e1
rohanshrma222 <hackerviruse04@gmail.com>
2025-09-10 15:41:23 +0530

    final

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,43 @@
+use borsh::{BorshDeserialize,BorshSerialize};
 use solana_program::{
-    account_info::{AccountInfo,next_account_info},
-    entrypoint::ProgramResult, entrypoint,
+    account_info::{AccountInfo,next_account_info}, 
+    entrypoint::{self, ProgramResult},
+    msg,
     pubkey::Pubkey,
-    msg
 };
+
+#[derive(BorshSerialize,BorshDeserialize)]
+enum InstructionType{
+    Increment(u32),
+    Decrement(u32)
+}
+
+#[derive(BorshSerialize,BorshDeserialize)]
+struct Counter {
+    count : u32,
+}
 
 entrypoint!(counter_contract);
 
 pub fn counter_contract(
     program_id: &Pubkey,
-    account: &[AccountInfo],
+    accounts: &[AccountInfo],
     instruction_data: &[u8]
 ) -> ProgramResult {
-    Ok(())
+     let acc = next_account_info(&mut accounts.iter())?;
+     let mut counter_data = Counter::try_from_slice(&acc.data.borrow())?;
+
+     let instruction_type = InstructionType::try_from_slice(instruction_data)?;
+     match instruction_type {
+          InstructionType::Increment(value)=> {
+            counter_data.count += value;
+          },
+          InstructionType::Decrement(value) =>{
+            counter_data.count -= value;
+          }
+     }
+     
+     counter_data.serialize(&mut *acc.data.borrow_mut());
+
+     Ok(())
 }
